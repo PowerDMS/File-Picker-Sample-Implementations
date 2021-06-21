ebf6d00ec89f7b389fa0707988a68277e449e47f
Chi Ho Yeung <chiho.yeung@powerdms.com>
2021-06-21 10:39:16 -0400

    added working sample implementation and updated readme

--- aspnetcore/FilePickerSample/wwwroot/js/site.js
+++ aspnetcore/FilePickerSample/wwwroot/js/site.js
@@ -2,24 +2,22 @@
     console.log("Javascript loaded.");
 })();
 
-function openFilePicker() {
-    var height = document.getElementById('input_height').value;
-    var width = document.getElementById('input_width').value;
+function openAuthModal() {
+    // Creating a state object with the username encoded. You can 
+    // add whatever other information you want here
+    const state = {
+        hash: 'fake-hash',
+        username: 'fake-username'
+    }
+    var encodedState = encodeURIComponent(btoa(JSON.stringify(state)));
 
     var config = {
-        apiKey: 'fake-api-key',
-        onSelection: displayResponse,
+        clientId: 'your-client-id',
+        redirectUrl: window.location.origin + '/callback',
+        state: encodedState,
     };
 
-    if (height) {
-        config.height = Number(height);
-    }
-
-    if (width) {
-        config.width = Number(width);
-    }
-
-    window.PowerDms.initializePowerDmsFilePicker(config);
+    window.PowerDms.openAuthModal(config);
 }
 
 function displayResponse(response) {
@@ -27,3 +25,28 @@ function displayResponse(response) {
     var message = 'FilePicker says: \n' + json;
     alert(message);
 }
+
+function receiveMessage(event) {
+    console.log(event);
+
+    if (event.origin !== 'https://filepicker.powerdms.com')
+    {
+        return;
+    }
+
+    var message = event.data;
+
+    if (message.type === 'DMS_FILEPICKER_SENDTOKENSTOHOST') {
+        const tokens = message.data;
+
+        const config = {
+            accessToken: tokens.accessToken,
+            idToken: tokens.idToken,
+            tokenRefreshUrl: window.location.origin + '/refresh',
+            onSelection: displayResponse,
+        };
+
+        window.PowerDms.openFilePicker(config);
+    }
+}
+window.addEventListener('message', receiveMessage, false);
